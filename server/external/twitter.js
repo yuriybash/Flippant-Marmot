49fcdb1cea9674c6ebe7c2f802a0099b481bf11e
Yuriy Bash <yuriybash@gmail.com>
2015-05-04 15:11:32 -0700

    adds Twitter API integration

---
+++ server/external/twitter.js
@@ -0,0 +1,47 @@
+var Twitter = require("twitter");
+
+//API credentials for our app
+var client = new Twitter({
+    consumer_key: 'Y5x8mOXZJOeKM02hSf7KW4MYS',
+    consumer_secret: '2uFdS0HmgssjRcsCN7JyDwXj1PYLrPWgmKSiEmQgpm10uKg5wD',
+    access_token_key: '2822221678-OSqIXMDn9MNNPIxC2PW47wUDPpNEeQIbmIXdTWi',
+    access_token_secret: 'CjbxFQWA7OXVZIqPEUXDRlpXdoeSNMN9TUcwSiyV6JF3d'
+});
+
+module.exports = {
+
+    /**
+     * This function returns Twitter data for one given handle.
+     * @constructor
+     * @param req title - request data for this request
+     * @param res author - response data for this response
+     */
+    getUserInfo: function(req, res) {
+        var twitterHandle = 'katyperry';//req.body.twitterHandle;
+        console.log(twitterHandle);
+
+        //response is JSON string of arrays. We will parse first result in it, create JSON from it, and send it back to client.
+        client.get('users/lookup', {
+            'screen_name': twitterHandle
+        }, function(error, response) {
+
+            var twitterUserData = {};
+
+            if (error) {
+                console.log("Error getting data from Twitter API");
+                res.send(404, "Sorry, bad Twitter handle - try again");
+            } else {
+            	var returnedUserData = JSON.parse(response)[0];
+                twitterUserData["screen_name"] = returnedUserData["screen_name"];
+                twitterUserData["name"] = returnedUserData["name"];
+                twitterUserData["follower_count_at_purchase"] = returnedUserData["followers_count"];
+                twitterUserData["price_at_purchase"] = parseInt(returnedUserData["followers_count"]) / 1000000;
+                res.send(200, JSON.stringify(twitterUserData));
+            }
+
+
+        })
+
+    }
+
+};
---
+++ server/external/twitterRoutes.js
@@ -0,0 +1,21 @@
+/**
+ * This file defines the routing when a request to '/api/twitter' is made. This router was 
+ * instantiated in middleware.js and is bound to 'api/twitter', so any URLs below are added
+ * as a suffix to 'api/twitter'. For example, when we define the route for '/' below,
+ * it's actually the route for www.socialstock.com/api/twitter.
+ *
+ * There is currently only one path, because we are currently only doing one thing with the Twitter API:
+ * requesting a specific Twitter users' information. Any new types of queries/API calls would be defined
+ * as new functions that live in twitter.js, and an additional routes for those calls would be listed here.
+ */
+
+//The actual API requests will live in twitter.js.
+var twitter = require('./twitter.js');
+
+//app is the twitterRouter injected from middleware.js
+module.exports = function(app){
+
+	app.post('/', twitter.getUserInfo);
+
+
+}
