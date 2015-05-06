8e48ae490f67703a67c8a8b848641edac1ceb52c
Yuriy Bash <yuriybash@gmail.com>
2015-05-05 19:28:07 -0700

    adds pricing algorithm

--- client/app/Factories/clientFactory.js
+++ client/app/Factories/clientFactory.js
@@ -6,27 +6,6 @@ angular.module('socialStock.factory', [])
 	.factory('clientFactory', function($http){
 
 
-		//gets portfolio for currently logged-in user
-		var getPortfolio = function () {
-		    return $http({
-		      method: 'GET',
-		      url: '/api/portfolio'
-		    })
-		    .then(function (resp) {
-		    	for(var i = 0; i < resp.stocks.length; i++){
-		    		var currentNumFollowers = this.getTwitterInfo({'twitterHandle': resp.stocks[i].screen_name}).follower_count_at_query_time;
-		    		var previousFollowers = resp.stocks[i].follower_count_at_purchase;
-		    		var numDays = Math.abs(new Date() - resp.stocks[i])/(1000*60*60*24);
-		    		var growthRate = Math.pow(currentNumFollowers/previousFollowers, 1/numDays) - 1;
-		    		var growthRateVsExpected = (growthRate - .0007)/.0007;
-		    		resp.stocks[i]["newPrice"] = growthRateVsExpected * resp.stocks[i].price_at_purchase;
-		    	}
-
-		   	  console.log('getPortfolio response: ', resp)
-		      return resp;
-		    });
-		  };
-
 		//gets Twitter stats for a specific handle; 
 		var getTwitterInfo = function (handle) {
 		    return $http({
@@ -38,6 +17,31 @@ angular.module('socialStock.factory', [])
 		      return resp;
 		    });
 		  };
+
+		//gets portfolio for currently logged-in user
+		var getPortfolio = function () {
+		    return $http({
+		      method: 'GET',
+		      url: '/api/portfolio'
+		    })
+		    .then(function (resp) {
+		    	if(resp.stocks.length > 0){
+			    	for(var i = 0; i < resp.stocks.length; i++){
+			    		console.log("GET PORTFOLIO RESPONSE GOES THROUGH")
+			    		var currentNumFollowers = this.getTwitterInfo({'twitterHandle': resp.stocks[i].screen_name}).follower_count_at_query_time;
+			    		var previousFollowers = resp.stocks[i].follower_count_at_purchase;
+			    		var numDays = Math.abs(new Date() - resp.stocks[i].date_of_purchase)/(1000*60*60*24);
+			    		var growthRate = Math.pow((currentNumFollowers-previousFollowers)/previousFollowers, 1/numDays) - 1;
+			    		var growthRateVsExpected = (growthRate - .0007)/.0007;
+			    		resp.stocks[i]["newPrice"] = (1+growthRateVsExpected) * resp.stocks[i].price_at_purchase;
+			    	}
+		    	}
+
+		   	  console.log('getPortfolio response: ', resp)
+		      return resp;
+		    });
+		  };
+
 
 		  var buyStock = function (stockInfo) {
 		      return $http({
