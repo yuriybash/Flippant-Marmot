7dee0d67f1c3dcef689e017378747c6e8e5ee45d
Kyle Hilton <kylehilton92@gmail.com>
2015-05-06 14:54:20 -0700

    adds search infrastructure

--- client/app/search/search.js
+++ client/app/search/search.js
@@ -3,5 +3,9 @@ angular.module('socialStock.search', [])
 .controller('SearchController', function ($scope, $location) {
   // Your code here
 
+  $scope.search = function(term){
+    console.log(term);
+  };
+
   console.log("inside searchController")
 });
