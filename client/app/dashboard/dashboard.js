4a13867a5c8c2cd6247b4256f7c069e948daee2c
Hack Reactor Students <accounts@hackreactor.com>
2015-05-06 19:03:06 -0700

    adds sell button logic

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -11,10 +11,41 @@ angular.module('socialStock.dash', [])
 
   clientFactory.getPortfolio().then(function(data){
     $scope.portfolio = data.data;
-    console.log($scope.portfolio);
   })
 
+  $scope.sell = function(sellStockInfo){
 
+    clientFactory.sellStock(sellStockInfo).then(function(data){
+      $scope.portfolio = data.data
+    })
+  }
+
+  var date = new Date();
+
+  $scope.createAndSell = function(sn, name, cfc, cd, cp, s){
+
+    var stockToSell = {
+        "screen_name": sn,
+        "name": name,
+        "current_follower_count": cfc,
+        "current_date": cd,
+        "current_price": cp,
+        "shares": s
+
+    }
+
+    $scope.sell(stockToSell);
+
+
+  }
+
+//   $scope.sellStockInfo = {
+//     "screen_name": $scope.stock.screen_name,
+//     "name": $scope.stock.name,
+//     "current_follower_count": 15000000,
+//     "current_date": date,
+//     "current_price": 15,
+//     "shares": 20 // need drop down menu!
+//   }
 });
-
 
