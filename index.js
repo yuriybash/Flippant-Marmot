6edc3c8e461286d50205bf31c2fa7a744f539808
Rocky Tang <rocky.tang168@gmail.com>
2015-05-08 16:07:27 -0700

    heroku deploy

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var app = require('./server/server.js')
 
-app.listen(3000);
-console.log("Now listening on port 3000")
+var port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log("Now listening on port", port)
