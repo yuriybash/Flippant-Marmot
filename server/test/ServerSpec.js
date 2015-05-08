9caf7b80f963ebeb6b4996556febc32223b2c616
FrankBowers24 <FrankBowers24@gmail.com>
2015-05-07 17:59:27 -0700

    Includes 5 authentication working authentication tests

--- server/test/ServerSpec.js
+++ server/test/ServerSpec.js
@@ -1,14 +1,7 @@
 var expect = require('chai').expect;
 var request = require('request');
-// var User = require('../users/userModel.js');
 var fs = require('fs');
 var path = require('path');
-
-// var db = require('../app/config');
-// var Users = require('../app/collections/users');
-// var User = require('../app/models/user');
-// var Links = require('../app/collections/links');
-// var Link = require('../app/models/link');
 
 /************************************************************/
 // Mocha doesn't have a way to designate pending before blocks.
@@ -16,326 +9,45 @@ var path = require('path');
 // Remove the 'x' from beforeEach block when working on
 // authentication tests.
 /************************************************************/
-var xbeforeEach = function(){};
+var xbeforeEach = function () {};
 /************************************************************/
 
 
-describe('', function() {
+describe('Priviledged Access:', function () {
 
-  beforeEach(function() {
-    // log out currently signed in user
-    request('http://localhost:3000/logout', function(error, res, body) {});
-
-/*    // delete link for roflzoo from db so it can be created later for the test
-    db.knex('urls')
-      .where('url', '=', 'http://www.roflzoo.com/')
-      .del()
-      .catch(function(error) {
-        throw {
-          type: 'DatabaseError',
-          message: 'Failed to create test setup data'
-        };
-      });
-
-    // delete user Svnh from db so it can be created later for the test
-    db.knex('users')
-      .where('username', '=', 'Svnh')
-      .del()
-      .catch(function(error) {
-        // uncomment when writing authentication tests
-        throw {
-          type: 'DatabaseError',
-          message: 'Failed to create test setup data'
-        };
-      });
-
-    // delete user Phillip from db so it can be created later for the test
-    db.knex('users')
-      .where('username', '=', 'Phillip')
-      .del()
-      .catch(function(error) {
-        // uncomment when writing authentication tests
-        throw {
-          type: 'DatabaseError',
-          message: 'Failed to create test setup data'
-        };
-      });*/
+  it('Redirects to login page if a user tries to access / and is not signed in', function (done) {
+    request('http://localhost:3000/', function (error, res, body) {
+      expect(res.req.path).to.equal('/signin.html'); // /login
+      done();
+    });
   });
 
-  xdescribe('Link creation:', function(){
-
-    var requestWithSession = request.defaults({jar: true});
-
-    beforeEach(function(done){      // create a user that we can then log-in with
-      new User({
-          'username': 'Phillip',
-          'password': 'Phillip'
-      }).save().then(function(){
-        var options = {
-          'method': 'POST',
-          'followAllRedirects': true,
-          'uri': 'http://localhost:3000/login',
-          'json': {
-            'username': 'Phillip',
-            'password': 'Phillip'
-          }
-        };
-        // login via form and save session info
-        requestWithSession(options, function(error, res, body) {
-          console.log('response to login',res);
-          done();
-        });
-      });
+  it('Redirects to login page if a user tries to go to /index.html and is not signed in', function (done) {
+    request('http://localhost:3000/index.html', function (error, res, body) {
+      expect(res.req.path).to.equal('/signin.html');
+      done();
     });
+  });
 
-    it('Only shortens valid urls, returning a 404 - Not found for invalid urls', function(done) {
-      var options = {
-        'method': 'POST',
-        'uri': 'http://localhost:3000/links',
-        'json': {
-          'url': 'definitely not a valid url'
-        }
-      };
-
-      requestWithSession(options, function(error, res, body) {
-        console.log('shorten urls test: ' + res.statusCode);
-        // res comes from the request module, and may not follow express conventions
-        expect(res.statusCode).to.equal(404);
-        done();
-      });
+  it('Portfolio api returns 401 if not signed in', function (done) {
+    request('http://localhost:3000/api/portfolio', function (error, res, body) {
+      expect(res.statusCode).to.equal(401);
+      done();
     });
+  });
 
-    describe('Shortening links:', function(){
-
-      var options = {
-        'method': 'POST',
-        'followAllRedirects': true,
-        'uri': 'http://localhost:3000/links',
-        'json': {
-          'url': 'http://www.roflzoo.com/'
-        }
-      };
-
-      it('Responds with the short code', function(done) {
-        requestWithSession(options, function(error, res, body) {
-          expect(res.body.url).to.equal('http://www.roflzoo.com/');
-          expect(res.body.code).to.not.be.null;
-          done();
-        });
-      });
-
-      it('New links create a database entry', function(done) {
-        requestWithSession(options, function(error, res, body) {
-          db.knex('urls')
-            .where('url', '=', 'http://www.roflzoo.com/')
-            .then(function(urls) {
-              if (urls['0'] && urls['0']['url']) {
-                var foundUrl = urls['0']['url'];
-              }
-              expect(foundUrl).to.equal('http://www.roflzoo.com/');
-              done();
-            });
-        });
-      });
-
-      it('Fetches the link url title', function (done) {
-        requestWithSession(options, function(error, res, body) {
-          db.knex('urls')
-            .where('title', '=', 'Funny pictures of animals, funny dog pictures')
-            .then(function(urls) {
-              console.log('urls inside',urls);
-              if (urls['0'] && urls['0']['title']) {
-                var foundTitle = urls['0']['title'];
-              }
-              expect(foundTitle).to.equal('Funny pictures of animals, funny dog pictures');
-              done();
-            });
-        });
-      });
-
-    }); // 'Shortening links'
-
-    describe('With previously saved urls:', function(){
-
-      var link;
-
-      beforeEach(function(done){
-        // save a link to the database
-        link = new Link({
-          url: 'http://www.roflzoo.com/',
-          title: 'Rofl Zoo - Daily funny animal pictures',
-          base_url: 'http://localhost:3000'
-        });
-        link.save().then(function(){
-          done();
-        });
-      });
-
-      it('Returns the same shortened code', function(done) {
-        var options = {
-          'method': 'POST',
-          'followAllRedirects': true,
-          'uri': 'http://localhost:3000/links',
-          'json': {
-            'url': 'http://www.roflzoo.com/'
-          }
-        };
-
-        requestWithSession(options, function(error, res, body) {
-          var code = res.body.code;
-          expect(code).to.equal(link.get('code'));
-          done();
-        });
-      });
-
-      it('Shortcode redirects to correct url', function(done) {
-        var options = {
-          'method': 'GET',
-          'uri': 'http://localhost:3000/' + link.get('code')
-        };
-
-        requestWithSession(options, function(error, res, body) {
-          var currentLocation = res.request.href;
-          expect(currentLocation).to.equal('http://roflzoo.com/');
-          done();
-        });
-      });
-
-      it('Returns all of the links to display on the links page', function(done) {
-        var options = {
-          'method': 'GET',
-          'uri': 'http://localhost:3000/links'
-        };
-
-        requestWithSession(options, function(error, res, body) {
-          expect(body).to.include('"title":"Rofl Zoo - Daily funny animal pictures"');
-          expect(body).to.include('"code":"' + link.get('code') + '"');
-          done();
-        });
-      });
-
-    }); // 'With previously saved urls'
-
-  }); // 'Link creation'
-
-  describe('Priviledged Access:', function(){
-
-    it('Redirects to login page if a user tries to access / and is not signed in', function(done) {
-      request('http://localhost:3000/', function(error, res, body) {
-        expect(res.req.path).to.equal('/signin.html');  // /login
-        done();
-      });
+  it('Twitter api returns 401 if not signed in', function (done) {
+    request('http://localhost:3000/api/twitter', function (error, res, body) {
+      expect(res.statusCode).to.equal(401);
+      done();
     });
+  });
 
-    it('Redirects to login page if a user tries to go to /index.html and is not signed in', function(done) {
-      request('http://localhost:3000/index.html', function(error, res, body) {
-        expect(res.req.path).to.equal('/signin.html');
-        done();
-      });
+  it('Users api returns 401 if not signed in', function (done) {
+    request('http://localhost:3000/api/users', function (error, res, body) {
+      expect(res.statusCode).to.equal(401);
+      done();
     });
+  });
 
-    it('Portfolio api returns 401 if not signed in', function(done) {
-      request('http://localhost:3000/api/portfolio', function(error, res, body) {
-        expect(res.statusCode).to.equal(401);
-        done();
-      });
-    });
-
-    it('Twitter api returns 401 if not signed in', function(done) {
-      request('http://localhost:3000/api/twitter', function(error, res, body) {
-        expect(res.statusCode).to.equal(401);
-        done();
-      });
-    });
-
-    it('Users api returns 401 if not signed in', function(done) {
-      request('http://localhost:3000/api/users', function(error, res, body) {
-        expect(res.statusCode).to.equal(401);
-        done();
-      });
-    });
-
-  }); // 'Priviledged Access'
-
-  describe('Twitter Login:', function(){
-
-    /*it('Twitter Login uses api.twitter.com', function(done) {
-      var options = {
-        'method': 'GET',
-        'uri': 'http://localhost:3000/auth/twitter',
-        'timeout': 5000
-      };
-
-      request(options, function(error, res, body) {
-        
-        // var filename = path.join(__dirname,'May7test.json');
-        // console.log('creating file: ', filename);
-        // fs.writeFileSync(filename, JSON.stringify(res, null, '\t'));
-        // console.log('COMPLETED LOGIN REQUEST: ', error, res, body);
-        expect(res.request.host).to.equal('api.twitter.com');
-        done();
-      });
-    });
-*/
-    // it('Twitter Login logs in a new user', function(done) {
-    //   var options = {
-    //     'method': 'GET',
-    //     'uri': '/auth/twitter'
-    //   };
-
-    //   request(options, function(error, res, body) {
-    //     expect(res.headers.location).to.equal('/');
-    //     done();
-    //   });
-    // });
-
-  }); // 'Account Creation'
-
-  xdescribe('Account Login:', function(){
-
-    var requestWithSession = request.defaults({jar: true});
-
-    beforeEach(function(done){
-      new User({
-          'username': 'Phillip',
-          'password': 'Phillip'
-      }).save().then(function(){
-        done()
-      });
-    })
-
-    it('Logs in existing users', function(done) {
-      var options = {
-        'method': 'POST',
-        'uri': 'http://localhost:3000/login',
-        'json': {
-          'username': 'Phillip',
-          'password': 'Phillip'
-        }
-      };
-
-      requestWithSession(options, function(error, res, body) {
-        expect(res.headers.location).to.equal('/');
-        done();
-      });
-    });
-
-    it('Users that do not exist are kept on login page', function(done) {
-      var options = {
-        'method': 'POST',
-        'uri': 'http://localhost:3000/login',
-        'json': {
-          'username': 'Fred',
-          'password': 'Fred'
-        }
-      };
-
-      requestWithSession(options, function(error, res, body) {
-        expect(res.headers.location).to.equal('/login');
-        done();
-      });
-    });
-
-  }); // 'Account Login'
-
-});
+}); // 'Priviledged Access'
