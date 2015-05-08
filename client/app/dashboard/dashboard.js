f47e31670269e6837673522bac8cc70130761733
FrankBowers24 <FrankBowers24@gmail.com>
2015-05-08 13:54:47 -0700

    Prepare stock data for piechart

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -7,6 +7,42 @@ angular.module('socialStock.dash', [])
     $scope.portfolio;
     $scope.networth;
 
+    $scope.updatePie = function(portfolio) {
+        var MAX_ASSETS = 4;
+        var values = [];
+        var labels = [];
+        var stocks = portfolio.stocks;
+        var netWorth = portfolio.cash_balance;
+        var stockValue = 0;
+        var assets = [];
+        values.push(portfolio.cash_balance);
+        labels.push('Cash');
+        // put all the stock assets in a name/value array
+        for (var i = 0; i < portfolio.stocks.length; i++) {
+            stockValue = stocks[i].current_price * stocks[i].shares;
+            netWorth += stockValue;
+            assets.push({name: stocks[i].screen_name, value: stockValue});
+        }
+        // sort by descending value
+        assets = assets.sort(function(a, b) {
+            return b.value - a.value;
+        });
+        console.log('sorted assets: ', JSON.stringify(assets));
+        var assetValue = 0;
+        for (var i = 0; i < assets.length && i < MAX_ASSETS; i++) {
+            values.push(assets[i].value);
+            labels.push(assets[i].name);
+            assetValue += assets[i].value;
+        }
+        var otherValue = netWorth - portfolio.cash_balance - assetValue;
+        if (otherValue > 0 && assets.length > MAX_ASSETS) {
+            values.push(otherValue);
+            labels.push('Other');
+        }
+        values.unshift(netWorth);
+        console.log('labels: ', labels, 'values: ', values);
+    }
+
 
     /**
      * This function refreshes the dashboard with the user's most recent portfolio data.
@@ -22,6 +58,7 @@ angular.module('socialStock.dash', [])
                 $scope.networth += data.data.stocks[i].current_price * data.data.stocks[i].shares;
             }
             $scope.networth += $scope.portfolio.cash_balance;
+            $scope.updatePie($scope.portfolio);
         })
     }
 
