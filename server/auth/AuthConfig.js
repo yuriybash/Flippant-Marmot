6bc72dfb04a491489cf549fa0501487ba4ea95ee
Rocky Tang <rocky.tang168@gmail.com>
2015-05-08 16:20:22 -0700

    adds AuthConfig

---
+++ server/auth/AuthConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  twitter: {
+    key: 'lau1z1hCNKkBI1O8zMJMG5kwP',
+    secret: 'PixxIaTinFWvOyg3MvhkoWRJU8lTQKhzb9TbsDPZL3ueOlGcS4'
+  },
+  facebook: {
+    key: '',
+    secret: ''
+  }
+};
