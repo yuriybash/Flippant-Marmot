78fb203eccaf8becc4561c7dc927a9e71d9e3448
Rocky Tang <rocky.tang168@gmail.com>
2015-05-08 17:08:33 -0700

    adds heroku mongodb database URI

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express();
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/socialstocks');
+mongoose.connect('mongodb://heroku_app36669420:ou53l12n7t6r8h9144oqrokpgu@ds061208.mongolab.com:61208/heroku_app36669420');
 
 require('./config/middleware.js')(app, express);
 
